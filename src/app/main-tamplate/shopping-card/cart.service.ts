c0954823f75ca066daa0e3485cb9f56432c10510
Petros X. <petrosxsp@gmail.com>
2024-11-13 23:34:27 +0200

    books and shopping cart services

---
+++ src/app/main-tamplate/shopping-card/cart.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Book } from '../books/book.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  private cartBooks: Book[] = [];
+  private cartBooks$ = new BehaviorSubject<Book[]>(this.cartBooks);
+  private idCounter = 1;
+
+  addToCart(book: Book) {
+    const bookWithId = { ...book, id: this.idCounter++ };
+    this.cartBooks.push(bookWithId);
+    this.cartBooks$.next(this.cartBooks);
+  }
+
+  removeFromCart(book: Book) {
+    const bookIndex = this.cartBooks.findIndex((b) => b.id === book.id);
+    if (bookIndex !== -1) {
+      this.cartBooks.splice(bookIndex, 1);
+      this.cartBooks$.next(this.cartBooks);
+    }
+  }
+
+  getCartBooks() {
+    return this.cartBooks$.asObservable();
+  }
+
+  getTotalCost(): number {
+    return this.cartBooks.reduce((total, book) => total + book.price, 0);
+  }
+}
--- src/app/main-tamplate/shopping-card/shopping-card.component.ts
+++ src/app/main-tamplate/shopping-card/shopping-card.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MainTamplateComponent } from '../main-tamplate.component';
+import { Book } from '../books/book.service';
+import { CartService } from './cart.service';
 
 @Component({
   selector: 'app-shopping-card',
@@ -8,4 +10,20 @@ import { MainTamplateComponent } from '../main-tamplate.component';
   templateUrl: './shopping-card.component.html',
   styleUrl: './shopping-card.component.scss',
 })
-export class ShoppingCardComponent {}
+export class ShoppingCardComponent implements OnInit {
+  cartBooks: Book[] = [];
+  totalCost: number = 0;
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.cartService.getCartBooks().subscribe((books) => {
+      this.cartBooks = books;
+      this.totalCost = this.cartService.getTotalCost();
+    });
+  }
+
+  removeBook(book: Book): void {
+    this.cartService.removeFromCart(book);
+  }
+}
