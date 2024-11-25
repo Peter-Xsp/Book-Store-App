1aee0436fb82302a0f7afd98e7e2fad347521545
Petros X. <petrosxsp@gmail.com>
2024-11-25 15:02:16 +0200

    environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://book-store-app.onrender.com/api',
+  apiUrl: 'https://book-store-app-vizt.onrender.com/api',
   //apiUrl: 'http://localhost:3000/api',
 };
