cd3ba8f1b2b6cfee8c1c3a1da150ae3ff505e8d4
Petros X. <petrosxsp@gmail.com>
2024-11-25 12:37:43 +0200

    environment r2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://Book-Store-App.onrender.com/api',
+  apiUrl: 'https://book-store-app.onrender.com',
   //apiUrl: 'http://localhost:3000/api',
 };
